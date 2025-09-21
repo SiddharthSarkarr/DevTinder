61089eaa88f5f8d0a5a25a5803c8cba327e9fb2d
SiddharthSarkarr <siddsarkarr@gmail.com>
2025-09-21 10:14:23 +0530

    middlewarea and error handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
+const { adminAuth } = require('./middleware/adminAuth');
 
+// next() concepts
 app.use("/user",
     (req,res, next) =>{
         console.log("heyy");
@@ -12,7 +14,66 @@ app.use("/user",
         console.log("hello")
         res.send("next function called")
     }
-)  
+) 
+// next() concepts
+
+
+// middleware concepts
+
+// app.use('/admin', adminAuth)
+
+app.get('/admin/getData',adminAuth, (req, res) =>{
+    // const token ='xyz';
+    // const isAuth = token === 'xyz';
+    // if(isAuth){
+    //     console.log("Data sent successfully");
+    //     res.send("Sent Data !!!");
+    // }else{
+    //     res.status(401).send("Unauthorized error")
+    // }
+    console.log("Data sent successfully");
+    res.send("Sent Data !!!");
+    
+})
+
+app.post('/admin/deleteData', (req, res) =>{
+    console.log("Data deleted successfully");
+    res.send("Deleted Data !!!");
+})
+
+// middleware concepts
+
+
+// error handling concepts
+
+// Method 1:
+    // app.get('/getUserData', (req, res) =>{
+    //     // logic to fetch data from Databse
+    //     throw new Error("static error - fcgnjd")
+    //     res.send("User data sent")
+    // })
+
+    // app.get('/', (err, req, res, next) =>{
+    //     if(err){
+    //         res.status(500).send("something went wrong")
+    //     }
+    // })
+
+// Method 2:
+app.get('/getUserData', (req, res) =>{
+    // logic to fetch data from Databse
+    try {
+        throw new Error("static error - fcgnjd")
+        res.send("User data sent")
+    } catch (error) {
+        res.status(500).send("something went wrong")
+    }
+    
+})
+
+// error handling concepts
+
+ 
 
 app.listen(1234, () =>{
     console.log("Server is successfully connected on port 1234.")
